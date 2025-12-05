5951307e100da8aeb39260513769b4d875968618
Bryan <bryanribero@hotmail.com>
2025-12-05 00:37:14 -0300

    practicando metodos y propiedades numericos

---
+++ practicas_de_pagina/numeros.js
@@ -0,0 +1,21 @@
+const num1 = '123Hola'
+const num2 = 2
+
+console.log(parseInt(num1) + num2)
+
+console.log(isNaN(parseInt(num1)))
+
+console.log(Math.round(6.35 * 10) / 10)
+
+function lectura() {
+  let numero
+
+  do {
+    numero = +prompt('Ingrese un numero:', '')
+  } while (isNaN(numero))
+
+  alert(numero)
+  return numero
+}
+
+lectura()
