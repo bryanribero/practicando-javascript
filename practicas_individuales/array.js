26b3e1f12b1a18fd407118dc5563390cbff6a67b
Bryan <bryanribero@hotmail.com>
2025-12-11 20:44:08 -0300

    Practicando con arrays

---
+++ practicas_individuales/array.js
@@ -0,0 +1,27 @@
+const arr1 = [1, 2, 3]
+
+const arr2 = [...arr1]
+
+const styles = ['jazz', 'blues']
+styles.push('rock-n-roll')
+styles[Math.floor((styles.length - 1) / 2)] = 'Classic'
+styles.shift()
+styles.unshift('rap', 'reggae')
+
+console.log(styles)
+
+const getMaxSubSum = (arr) => {
+  let maxSum = 0
+  let numeroActual = 0
+
+  for (const item of arr) {
+    numeroActual += item
+
+    maxSum = Math.max(maxSum, numeroActual)
+
+    if (numeroActual < 0) numeroActual = 0
+  }
+  return maxSum
+}
+
+console.log(getMaxSubSum([100, -9, 2, -3, 5]))
