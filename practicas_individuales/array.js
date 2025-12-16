343aa988cf7fe3b28955d4a879c75a9dce82c177
Bryan <bryanribero@hotmail.com>
2025-12-16 18:54:54 -0300

    Practicando arrays

--- practicas_individuales/array.js
+++ practicas_individuales/array.js
@@ -29,7 +29,27 @@ console.log(getMaxSubSum([100, -9, 2, -3, 5]))
 const camelize = (str) =>
   str
     .split('-')
-    .map((element, index) => (index == 0 ? element : element[0].toUpperCase() + element.slice(1)))
-    .join('')
+    .map((e, i) => (i === 0 ? e : e[0].toUpperCase() + e.slice(1)))
+    .join('-')
 
-console.log(camelize('list-style-image'))
+console.log(camelize('background-color'))
+
+function filterRange(arr, a, b) {
+  return arr.filter((e) => e >= a && e <= b)
+}
+
+console.log(filterRange([1, 2, 3, 4, 5, 6, 7, 8], 4, 6))
+
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    const value = arr[i]
+
+    if (value < a || value > b) {
+      arr.splice(i, 1)
+      i--
+    }
+  }
+  return arr.sort()
+}
+
+console.log(filterRangeInPlace([5, 3, 8, 1], 1, 4))
