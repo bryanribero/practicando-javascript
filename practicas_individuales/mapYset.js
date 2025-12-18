1565f034f05d98eec19169f9810a24f8b92018f7
Bryan <bryanribero@hotmail.com>
2025-12-17 23:48:23 -0300

    Usar map para guardar un mismo conjunto de letras ordenandolas

--- practicas_individuales/mapYset.js
+++ practicas_individuales/mapYset.js
@@ -1,9 +1,21 @@
-function unique(arr) {
-  const seteo = new Set(arr)
-
-  return seteo
-}
+const unique = (arr) => Array.from(new Set(arr))
 
 let values = ['Hare', 'Krishna', 'Hare', 'Krishna', 'Krishna', 'Krishna', 'Hare', 'Hare', ':-O']
 
 console.log(unique(values))
+
+const aclean = (arr) => {
+  let map = new Map()
+
+  for (let word of arr) {
+    const sorted = word.toLowerCase().split('').sort().join('')
+
+    map.set(sorted, word)
+  }
+
+  return Array.from(map.values())
+}
+
+let arr = ['nap', 'teachers', 'cheaters', 'PAN', 'ear', 'era', 'hectares']
+
+console.log(aclean(arr))
