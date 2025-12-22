35df5c88b74c5d4be529268beef75162d54fbc41
Bryan <bryanribero@hotmail.com>
2025-12-21 22:39:26 -0300

    Seteando un WeakMap para que tenga la fecha de cuando se leyo el mensaje

--- practicas_individuales/weakMapYWeakSet.js
+++ practicas_individuales/weakMapYWeakSet.js
@@ -8,6 +8,14 @@ const readMessages = new WeakSet()
 readMessages.add(messages[0])
 readMessages.add(messages[1])
 
-messages.shift()
-
 console.log(`Read message 1: ${readMessages.has(messages[0])}`)
+
+const date = new Date()
+
+const readMessagesDate = new WeakMap()
+
+for (const obj of messages) {
+  readMessagesDate.set(obj, date)
+}
+
+console.log(readMessagesDate.get(messages[1]))
