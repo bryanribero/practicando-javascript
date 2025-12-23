079eea66c4f57dd96aa62b2e137e58a72b813cc9
Bryan <bryanribero@hotmail.com>
2025-12-23 20:49:56 -0300

    Practicando desestructuracion en un bucle for

--- practicas_individuales/desestructuracion.js
+++ practicas_individuales/desestructuracion.js
@@ -1,10 +1,32 @@
 let user = {
-  namee: 'John',
+  name: 'John',
   years: 30
 }
 
-let { namee, years, isAdmin = false } = user
+let { name: nombre, years: age, isAdmin = false } = user
 
-console.log(namee)
-console.log(years)
+console.log(nombre)
+console.log(age)
 console.log(isAdmin)
+
+function topSalary(salaries) {
+  let maxSalary = 0
+  let maxName = null
+
+  for (const [name, salary] of Object.entries(salaries)) {
+    if (maxSalary < salary) {
+      maxSalary = salary
+      maxName = name
+    }
+  }
+
+  return maxName
+}
+
+let salaries = {
+  John: 100,
+  Pete: 300,
+  Mary: 250
+}
+
+console.log(topSalary(salaries))
