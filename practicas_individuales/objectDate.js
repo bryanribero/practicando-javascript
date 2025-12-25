48f905e2a4d8e8009c84872259b662c34bae9887
Bryan <bryanribero@hotmail.com>
2025-12-24 21:44:03 -0300

    comprobando el funcionamiento de Date.now

---
+++ practicas_individuales/objectDate.js
@@ -0,0 +1,26 @@
+let fecha = new Date()
+console.log(fecha.toString())
+
+console.log(fecha.getTimezoneOffset())
+
+fecha.setHours(5)
+
+console.log(fecha.getHours())
+
+let horario = new Date(2020, 0, 1)
+
+horario.setDate(0)
+
+console.log(horario.toString())
+
+console.log(+horario)
+
+const start = Date.now()
+
+for (let i = 0; i < 100000; i++) {
+  let doSomething = i * i * i
+}
+
+const end = Date.now()
+
+console.log(`El bucle tardo ${end - start} ms`)
