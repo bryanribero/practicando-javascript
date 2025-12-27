b4c28535c05ceeb6ca82f8215076da158f3fe695
Bryan <bryanribero@hotmail.com>
2025-12-26 22:51:56 -0300

    Creacion de funcion que te muestra el dia del date que le pases

--- practicas_individuales/objectDate.js
+++ practicas_individuales/objectDate.js
@@ -1,19 +1,19 @@
 let fecha = new Date()
-console.log(fecha.toString())
+// console.log(fecha.toString())
 
-console.log(fecha.getTimezoneOffset())
+// console.log(fecha.getTimezoneOffset())
 
 fecha.setHours(5)
 
-console.log(fecha.getHours())
+// console.log(fecha.getHours())
 
 let horario = new Date(2020, 0, 1)
 
 horario.setDate(0)
 
-console.log(horario.toString())
+// console.log(horario.toString())
 
-console.log(+horario)
+// console.log(+horario)
 
 const start = Date.now()
 
@@ -23,10 +23,21 @@ for (let i = 0; i < 100000; i++) {
 
 const end = Date.now()
 
-console.log(`El bucle tardo ${end - start} ms`)
+// console.log(`El bucle tardo ${end - start} ms`)
 
-console.log(start)
+// console.log(start)
 
-const fechaActual = new Date(2012, 1, 20, 3, 12)
+const fechaActual = new Date(2012, 1, 6, 3, 12)
 
 console.log(fechaActual.toString())
+console.log(fechaActual.getDay())
+
+function getLocalDay(date) {
+  const days = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']
+
+  return days[date.getDay()]
+}
+
+let date2 = new Date(2012, 0, 3)
+
+console.log(getLocalDay(date2))
