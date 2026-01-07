d0b8daee656acd62404ac7df12a35ee2681df02c
Bryan <bryanribero@hotmail.com>
2026-01-06 23:47:38 -0300

    probando usar promesas con then y catch

--- miduDev/async.js
+++ miduDev/async.js
@@ -3,7 +3,9 @@ import fs from 'fs/promises' //Necesario importar las promesas
 async function getData() {
   const data = await fs.readFile('dato.txt', 'utf-8')
 
-  console.log(data)
+  return data
 }
 
-getData()
+getData().then((data) => {
+  console.log(data)
+})
