8442f8ac2e8a947108039af7391d8410dae72dcb
Bryan <bryanribero@hotmail.com>
2026-01-07 23:41:34 -0300

    agregando catch

--- miduDev/async.js
+++ miduDev/async.js
@@ -1,11 +1,13 @@
 import fs from 'fs/promises' //Necesario importar las promesas
 
 async function getData() {
-  const data = await fs.readFile('dato.txt', 'utf-8')
+  const data = await fs.readFile('dat.txt', 'utf-8')
 
   return data
 }
 
-getData().then((data) => {
-  console.log(data)
-})
+getData()
+  .then((data) => {
+    console.log(data)
+  })
+  .catch((err) => console.log('el error es:' + err))
