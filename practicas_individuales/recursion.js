35924fca707e87c293d395164636b6bc21e9eee8
Bryan <bryanribero@hotmail.com>
2026-01-26 22:47:22 -0300

    Creando una funcion para ir sumando una cantidad de numeros

--- practicas_individuales/recursion.js
+++ practicas_individuales/recursion.js
@@ -2,4 +2,13 @@ function pow(x, n) {
   return n == 1 ? x : x * pow(x, n - 1)
 }
 
-console.log(pow(2, 3))
+// console.log(pow(2, 3))
+function sumTo(x) {
+  let sum = 0
+  for (let i = x; i > 0; i--) {
+    sum += i
+  }
+  return sum
+}
+
+console.log(sumTo(4))
