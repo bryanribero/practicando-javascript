0e791d8b0dd135cd8f2847d0fdca37fbcb7410e8
Bryan <bryanribero@hotmail.com>
2026-01-27 22:43:16 -0300

    PrintList hecho con bucle de una lista de un solo enlace

---
+++ practicas_individuales/listaTarea.js
@@ -0,0 +1,24 @@
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null
+      }
+    }
+  }
+}
+
+function printListBucle(list) {
+  let tmp = list
+
+  while (tmp) {
+    console.log(tmp.value)
+    tmp = tmp.next
+  }
+}
+
+printListBucle(list)
