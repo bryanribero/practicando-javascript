9617fb005f0dedf52dbc4c7f16277783b1b41b33
Bryan <bryanribero@hotmail.com>
2026-01-29 19:44:51 -0300

    Practicando mas el entornoLexico

--- practicas_individuales/entornoLexico.js
+++ practicas_individuales/entornoLexico.js
@@ -1,4 +1,4 @@
-let name = 'Jhon'
+/* let name = 'Jhon'
 
 function sayHi() {
   console.log(`Hi ${name}`)
@@ -7,3 +7,24 @@ function sayHi() {
 name = 'Pedro'
 
 sayHi()
+
+name = 'Pablito'
+
+ */
+
+/* El entorno lexico de la funcion retornada busca una variable exterior a ella ( name ) y como se encuentra en la function 
+makeWorker tomara ese valor, en caso de que no exista tomaria el valor de name pedro */
+
+function makeWorker() {
+  let name = 'Jhon'
+
+  return function () {
+    console.log(name)
+  }
+}
+
+let name = 'Pedro'
+
+let work = makeWorker()
+
+work()
