c8c82131493fbc3a818fe30e90a324fd2a721ba2
Bryan <bryanribero@hotmail.com>
2026-02-01 18:37:20 -0300

    Creando un wrapper para una funcion con metodo y con mas de un argumento manteniendo el this de la funcion original

--- practicas_individuales/thisContext.js
+++ practicas_individuales/thisContext.js
@@ -7,3 +7,30 @@ let persona = {
 }
 
 sayHi.call(persona, 'buenos dias.')
+
+function saludoDoble() {}
+
+saludoDoble.sayHi = function (n1, n2) {
+  return `Hola ${n1} y ${n2}`
+}
+
+function decorating(func) {
+  let cache = new Map()
+
+  return function (...args) {
+    let key = JSON.stringify(args)
+
+    if (cache.has(key)) {
+      return cache.get(key)
+    }
+
+    let result = func.call(this, ...args)
+
+    cache.set(key, result)
+    return result
+  }
+}
+
+saludoDoble.sayHi = decorating(saludoDoble.sayHi)
+
+console.log(saludoDoble.sayHi('Pedro', 'Carlos'))
