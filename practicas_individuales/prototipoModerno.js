6e623d86ecc8f8226572ab0cab9e0103ec4ffcbe
Bryan <bryanribero@hotmail.com>
2026-02-12 21:07:31 -0300

    Ejercicio utilizando Object.create para que no tenga prototype y creandole el metdo toString

---
+++ practicas_individuales/prototipoModerno.js
@@ -0,0 +1,19 @@
+let dictionary = Object.create(null, {
+  toString: {
+    value() {
+      return Object.keys(this).join()
+    }
+  }
+})
+
+// agregar algunos datos
+dictionary.apple = 'Manzana'
+dictionary.__proto__ = 'prueba' // // aquí proto es una propiedad clave común
+
+// solo manzana y __proto__ están en el ciclo
+for (let key in dictionary) {
+  console.log(key) // "manzana", después "__proto__"
+}
+
+// tu toString en acción
+console.log(String(dictionary))
