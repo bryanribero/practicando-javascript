1dc638831ec6c93b2ce9d38edd0a79d54c175b46
Bryan <bryanribero@hotmail.com>
2026-02-13 15:36:37 -0300

    practicando prototipos con set y get prototype

--- practicas_individuales/prototipoModerno.js
+++ practicas_individuales/prototipoModerno.js
@@ -10,6 +10,11 @@ let dictionary = Object.create(null, {
 dictionary.apple = 'Manzana'
 dictionary.__proto__ = 'prueba' // // aquí proto es una propiedad clave común
 
+let perro = {
+  ladrar: 'guf'
+}
+
+Object.setPrototypeOf(dictionary, perro)
 // solo manzana y __proto__ están en el ciclo
 for (let key in dictionary) {
   console.log(key) // "manzana", después "__proto__"
@@ -17,3 +22,5 @@ for (let key in dictionary) {
 
 // tu toString en acción
 console.log(String(dictionary))
+
+console.log(Object.getPrototypeOf(dictionary))
